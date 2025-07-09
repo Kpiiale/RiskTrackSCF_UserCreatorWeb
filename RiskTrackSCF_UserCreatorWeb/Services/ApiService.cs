2bc2cef138dbed5975c038287d95cff7c4ca4edf
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-09 02:01:48 -0500

    Add ApiService

---
+++ RiskTrackSCF_UserCreatorWeb/Services/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using RiskTrackSCF_UserCreatorWeb.Models;
+
+namespace RiskTrackSCF_UserCreatorWeb.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _http;
+
+        public ApiService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task CreateCompanyAsync(Company company)
+        {
+            await _http.PostAsJsonAsync("api/companies", company);
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _http.PostAsJsonAsync("api/users", user);
+        }
+    }
+}
