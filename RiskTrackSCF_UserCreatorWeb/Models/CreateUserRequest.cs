ea3f900a51db536ec006acd2513a7cafa0dd8d94
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-10 06:06:05 -0500

    Add Models For auth and user creation

---
+++ RiskTrackSCF_UserCreatorWeb/Models/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace RiskTrackSCF_UserCreatorWeb.Models
+{
+    public class CreateUserRequest
+    {
+        public int? CompanyId { get; set; }  
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "U";
+    }
+
+}
---
+++ RiskTrackSCF_UserCreatorWeb/Models/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace RiskTrackSCF_UserCreatorWeb.Models
+{
+    public class UserSession
+    {
+        public int UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Role { get; set; }
+    }
+}
