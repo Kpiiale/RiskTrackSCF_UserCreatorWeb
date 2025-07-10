21cd488731f2e95924aca476888f8629144948f7
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-10 06:09:50 -0500

    Add AuthController

---
+++ RiskTrackSCF_UserCreatorWeb/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RiskTrackSCF_UserCreatorWeb.Models;
+
+namespace RiskTrackSCF_UserCreatorWeb.Services
+{
+    public class AuthService
+    {
+        public UserSession? CurrentUser { get; private set; }
+
+        public bool IsAuthenticated => CurrentUser != null;
+        public bool IsAdmin => CurrentUser?.Role == "A";
+
+        public void Login(User user)
+        {
+            CurrentUser = new UserSession
+            {
+                Username = user.Username,
+                Role = user.Role
+            };
+        }
+
+        public void Logout()
+        {
+            CurrentUser = null;
+        }
+    }
+}
